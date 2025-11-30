c6c481a9a4a4a539cfb38535d757f57bafd4b85c
HENRY596196 <huaug9672@gmail.com>
2025-12-01 01:27:23 +0800

    Add files via upload

---
+++ counter.js
@@ -0,0 +1,16 @@
+// // 讀取本地儲存的次數，如果沒有就設 0
+// let counter = localStorage.getItem('visitCount') || 0;
+
+// // 次數加 1
+// counter++;
+
+// // 存回 localStorage
+// localStorage.setItem('visitCount', counter);
+
+// // 顯示在頁面
+// window.addEventListener('DOMContentLoaded', () => {
+//     const counterElement = document.getElementById('counter');
+//     if (counterElement) {
+//         counterElement.innerText = counter;
+//     }
+// });
